66b043508b741d46487967cc9034b2cc9970adba
Mo-jaji <mohammadjaji98@gmail.com>
2022-12-13 11:45:51 +0000

    added the outputs

--- Code/vpc_module.tf
+++ Code/vpc_module.tf
@@ -3,7 +3,7 @@ module "my_vpc" { # module are attained from terraform website which give an exa
   version = "3.18.1"
 
   # VPC Basic Details
-  name                = "${local.name}-${var.vpc_name}"
+  name                = "${local.name}-${var.vpc_name}" # will produce the followling name HR-Dev-my_vpc
   cidr                = var.vpc_cidr_block  
   azs                 = var.vpc_availability_zones
   private_subnets     = var.vpc_public_subnets
@@ -15,4 +15,27 @@ module "my_vpc" { # module are attained from terraform website which give an exa
   create_database_subnet_group       = var.vpc_create_database_subnet_group
   create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
 
+  # NAT gatways  -outbound communicaiton
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+  single_nat_gateway = var.vpc_single_nat_gateway
+
+  # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags     = local.common_tags
+  vpc_tags = local.common_tags
+
+  # Additional Tags to Subnets
+  public_subnet_tags = {
+    Type = "Public Subnets"
+  }
+  private_subnet_tags = {
+    Type = "Private Subnets"
+  }
+  database_subnet_tags = {
+    Type = "Private Database Subnets"
+  }
 }
+
--- Code/vpc_vars.tf
+++ Code/vpc_vars.tf
@@ -5,7 +5,7 @@ variable "vpc_name" {
   default     = "my_vpc"
 }
 
-# VPC CIDR Block
+# 
 variable "vpc_cidr_block" {
   description = "VPC CIDR Block"
   type        = string

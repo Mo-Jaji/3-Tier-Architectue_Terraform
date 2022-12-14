39e13e0f28b8e04e38939b6d93cd78c635537d16
Mo-jaji <mohammadjaji98@gmail.com>
2022-12-14 17:02:03 +0000

    ec2 public module

---
+++ Code/ec2_public_module.tf
@@ -0,0 +1,23 @@
+module "my_public_ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.2.1"
+
+ # insert the 34 required variables here
+
+  name = "${var.my_environment}-Public_EC2_Intance"
+  # instance_count = 5 as it only for one instance we dont need count but I have added for future 
+
+  ami           = data.aws_ami.my_amzlinux2.id
+  instance_type = var.instance_type
+  key_name      = var.instance_keypair
+  # monitoring             = true
+  vpc_security_group_ids = [module.my_public_bastion_SG.this_security_group_id]
+  subnet_id              = module.my_vpc.public_subnets[0]
+  user_data = file("${path.module}/app1-install.sh")
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+}

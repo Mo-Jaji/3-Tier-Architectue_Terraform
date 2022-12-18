48ed13c66e38a9770bbd67909db36827181f5a0c
Mo-jaji <mohammadjaji98@gmail.com>
2022-12-18 14:33:14 +0000

    .

--- Code/provider.tf
+++ Code/provider.tf
@@ -5,5 +5,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+     null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
   }
+}
+}
+
+provider "aws" {
+  region = var.my_aws_region
 }
